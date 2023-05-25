b73cd995743eeb11f83c28b896ee74f9da73616b
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2023-05-25 18:43:33 -0300

    problema de cors

--- src/context.js
+++ src/context.js
@@ -1,36 +1,35 @@
 const app = require('./app');
-const cors = require('cors')
+const cors = require('cors');
+const helmet  = require('helmet');
 const port = process.env.PORT || 5000;
 const AlunoController = require('./controllers/alunos.controller');
-require('dotenv').config({  
-    path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env"
-  })
+require('dotenv').config({
+  path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env"
+})
+
+app.use(cors());
 
 const knex = require('knex')({
-    client: 'mysql2',
-    connection: 'mysql://root:1234@localhost:3306/trabalho_s',
-    pool: {
-        min: 2,
-        max: 10,
-    }
+  client: 'mysql2',
+  connection: 'mysql://root:1234@localhost:3306/trabalho_s',
+  pool: {
+    min: 2,
+    max: 10,
+  }
 });
 
 const routes = require('./routes/index.routes')
-app.use('/api',routes);
+app.use('/api', routes);
 
 
 // app.use(routes)
 
-app.use(cors(
-    {
-      origin: '*',
-    }
-  ));
+
 //   app.get('/test',async (req,res) => res.json({teste:'dd'}))
 // app.get(('/api/aluno',(req,res) => res.json('')))
 
 app.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}/`);
+  console.log(`Server is running on port http://localhost:${port}/`);
 });
 
 module.exports = knex;
